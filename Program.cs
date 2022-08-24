64e7fc956ff86d0112211172ceeda4c394ab35b0
Dmitry <vip.mihaslw@mail>
2022-08-24 14:38:16 +0300

    ура

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите ваше имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("ура, это же Маша");
+} 
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
